96339bb30c48b754b70af0956fdd57f5e68e8ea6
Rafael Chaves <rafaelguichaves@gmail.com>
2022-02-22 16:53:55 -0300

    Product memory

--- src/main/java/com/devsuperior/myfirstproject/entities/Category.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Category.java
@@ -1,12 +1,19 @@
 package com.devsuperior.myfirstproject.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	private List<Product> products = new ArrayList<>();
 	
 	public Category () {
 		
@@ -32,6 +39,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public List<Product> getProducts() {
+		return products;
 	}
 
 	@Override
